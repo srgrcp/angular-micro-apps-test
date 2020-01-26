d47e5fe20e0e79cbaf6b281a84beaa100fc71138
Sergio Carrillo <sergiocarri95@gmail.com>
2020-01-25 21:48:22 -0500

    Updating

---
+++ libs/service/src/lib/service.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ServiceComponent } from './service.component';
+
+@NgModule({
+  declarations: [ServiceComponent],
+  imports: [
+  ],
+  exports: [ServiceComponent]
+})
+export class ServiceModule { }
---
+++ libs/service/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of service
+ */
+
+export * from './lib/service.service';
+export * from './lib/service.component';
+export * from './lib/service.module';
